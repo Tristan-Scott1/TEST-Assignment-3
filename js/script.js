20f29061107048f870cdbc2aed70990f66a4c02c
tristan-scott <scottt@uleth.ca>
2023-12-03 13:16:24 -0700

    TESTING: add buttons for phases of anim

--- js/script.js
+++ js/script.js
@@ -5,10 +5,51 @@ function getCurrentIndexImage(value){
     return  value;
 }
 
-function animate() {
+//=======================================================
+function animate1() {
     requestAnimationFrame(() => {
         setTimeout(() => {
-            animate();
+            animate1();
+        }, 1000 / 10);
+    });
+
+    const image = document.getElementById('imageRender');
+    image.src='../images/AnimationTest-' + getCurrentIndexImage(currentImageIndex) + '.png';
+    if(currentImageIndex < 5){
+        currentImageIndex++;
+    } else {
+        currentImageIndex = 1;
+    }
+}
+
+function startAnimate1() {
+    animate1();
+}
+//=======================================================
+function animate2() {
+    requestAnimationFrame(() => {
+        setTimeout(() => {
+            animate2();
+        }, 1000 / 10);
+    });
+
+    const image = document.getElementById('imageRender');
+    image.src='../images/AnimationTest-' + getCurrentIndexImage(currentImageIndex) + '.png';
+    if(currentImageIndex < 12){
+        currentImageIndex++;
+    } else {
+        currentImageIndex = 6;
+    }
+}
+
+function startAnimate2() {
+    animate2();
+}
+//=======================================================
+function animate3() {
+    requestAnimationFrame(() => {
+        setTimeout(() => {
+            animate3();
         }, 1000 / 10);
     });
 
@@ -17,10 +58,11 @@ function animate() {
     if(currentImageIndex < 20){
         currentImageIndex++;
     } else {
-        currentImageIndex = 1;
+        currentImageIndex = 13;
     }
 }
 
-function startAnimate() {
-    animate();
+function startAnimate3() {
+    animate3();
 }
+
